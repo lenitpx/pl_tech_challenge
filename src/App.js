0ea76607ffdb1d0c789dfabd394dbbcfb6e2b1e2
HTarzwell <htarzwell@gmail.com>
2020-12-10 18:32:56 -0500

    latest iteration

--- src/App.js
+++ src/App.js
@@ -2,39 +2,71 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import {useState, useEffect} from 'react';
-import { Card, Icon } from 'semantic-ui-react';
+import { Card, Icon, Image } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
 
-  const [employee, getEmployee] = useState('');
+  const [employees, getEmployee] = useState('');
 
   useEffect(() => {
-    function getTheThing() {
+    function getEmployees() {
       axios.get('https://randomuser.me/api/1.3?results=50')
     .then((response) => {
-      const employeeRando = response.data.results
+      const employeesReturned = response.data.results
       console.log(response.data.results[3])
-      getEmployee(employeeRando);
+      getEmployee(employeesReturned);
     })
     .catch((error) => {
       console.log(error);
     })}
 
-    getTheThing();
-  }, {});
+    getEmployees();
+  }, []);
+
+  const extra = employee => {
+    return (
+      <a>
+        <Icon name="flag outline"/>
+        {employee.nat}
+      </a>
+    )
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Meet our employees!</p>
-        {employee.length > 0 ?
-        <Card
+        <Card.Group>
+        {employees.length > 0 ?
 
-          header='its cha boi'
-        /> :
+          employees.map((employee, index) => {
+            return (
+              <Card key={index}>
+                <Card.Content textAlign="left">
+                  <Image
+                    floated='right'
+                    size='mini'
+                    src={employee.picture.thumbnail}
+                  />
+                  <Card.Header>{employee.name.first} {employee.name.last}</Card.Header>
+                </Card.Content>
+                <Card.Content textAlign="left">
+                  <Card.Meta>Located in {employee.location.city}</Card.Meta>
+                  <Card.Description>
+                    <Icon size="tiny" name="phone" />
+                      {employee.phone}
+                    <Icon size="tiny" name="mail" />
+                      <p>{employee.email}</p>
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            )
+          })
+         :
         <p>there was a problem.  come back soon!</p>
         }
+        </Card.Group>
       </header>
     </div>
   );
