a29573e4f1078d9409b6353cdf1898a240e52ec3
HTarzwell <htarzwell@gmail.com>
2020-12-10 17:30:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import { Card, Icon } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
+
+  const [employee, getEmployee] = useState('');
+
+  useEffect(() => {
+    function getTheThing() {
+      axios.get('https://randomuser.me/api/1.3?results=50')
+    .then((response) => {
+      const employeeRando = response.data.results
+      console.log(response.data.results[3])
+      getEmployee(employeeRando);
+    })
+    .catch((error) => {
+      console.log(error);
+    })}
+
+    getTheThing();
+  }, {});
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Meet our employees!</p>
+        {employee.length > 0 ?
+        <Card
+
+          header='its cha boi'
+        /> :
+        <p>there was a problem.  come back soon!</p>
+        }
       </header>
     </div>
   );
