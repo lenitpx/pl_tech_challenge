4301c1bff2dfada65260fe844039c1c260e51d16
HTarzwell <htarzwell@gmail.com>
2020-12-11 19:50:27 -0500

    fixed styling and instability broke out options

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import {useState, useEffect} from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css'
+import { Dropdown } from 'semantic-ui-react';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Browse from './Browse/Browse'
+import Search from './Search/Search'
+import Sort from './Sort/Sort'
 
 function App() {
 
@@ -14,7 +21,6 @@ function App() {
       axios.get('https://randomuser.me/api/1.3?results=50')
     .then((response) => {
       const employeesReturned = response.data.results
-      console.log(response.data.results[3])
       getEmployee(employeesReturned);
     })
     .catch((error) => {
@@ -24,50 +30,29 @@ function App() {
     getEmployees();
   }, []);
 
-  const extra = employee => {
-    return (
-      <a>
-        <Icon name="flag outline"/>
-        {employee.nat}
-      </a>
-    )
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Meet our employees!</p>
-        <Card.Group>
-        {employees.length > 0 ?
-
-          employees.map((employee, index) => {
-            return (
-              <Card key={index}>
-                <Card.Content textAlign="left">
-                  <Image
-                    floated='right'
-                    size='mini'
-                    src={employee.picture.thumbnail}
-                  />
-                  <Card.Header>{employee.name.first} {employee.name.last}</Card.Header>
-                </Card.Content>
-                <Card.Content textAlign="left">
-                  <Card.Meta>Located in {employee.location.city}</Card.Meta>
-                  <Card.Description>
-                    <Icon size="tiny" name="phone" />
-                      {employee.phone}
-                    <Icon size="tiny" name="mail" />
-                      <p>{employee.email}</p>
-                  </Card.Description>
-                </Card.Content>
-              </Card>
-            )
-          })
-         :
-        <p>there was a problem.  come back soon!</p>
-        }
-        </Card.Group>
-      </header>
+      <HashRouter>
+        <header className="App-header">
+          <h1><NavLink exact activeClassName="active" to="/">Employee Directory</NavLink></h1>
+          <ul>
+            <li><NavLink exact activeClassName="active" to="/">Browse</NavLink></li>
+            <li><NavLink exact activeClassName="active" to="/sort">Sort</NavLink></li>
+            <li><NavLink exact activeClassName="active" to="/search">Search</NavLink></li>
+          </ul>
+        </header>
+      <div className="App-body">
+        <Route exact path="/" render={() => (
+          <Browse employees={employees} />
+        )}/>
+        <Route exact path="/sort" render={() => (
+          <Sort employees={employees} />
+        )}/>
+        <Route exact path="/search" render={() => (
+          <Search employees={employees} />
+        )}/>
+      </div>
+      </HashRouter>
     </div>
   );
 }
